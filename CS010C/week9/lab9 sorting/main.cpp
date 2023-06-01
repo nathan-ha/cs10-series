074e07e33788859a9df0bf3b2a21b5afa1488d3f
Nathan <nathan_ha@outlook.com>
2023-06-01 15:29:29 -0700

    finished sorting lab

--- CS010C/week9/lab9 sorting/main.cpp
+++ CS010C/week9/lab9 sorting/main.cpp
@@ -8,41 +8,63 @@ const int NUMBERS_SIZE = 50000;
 int genRandInt(int low, int high);
 void fillArrays(int arr1[], int arr2[], int arr3[]);
 
-void Quicksort_midpoint(int numbers[], int i, int k);
-void Quicksort_medianOfThree(int numbers[], int i, int k);
+void Quicksort_midpoint(int numbers[], int low, int high);
+void Quicksort_medianOfThree(int numbers[], int low, int high);
 void InsertionSort(int numbers[], int numbersSize);
+int Partition(int numbers[], int low, int high, bool usesMedianOfThree);
 
 int main()
 {
+    const int CLOCKS_PER_MS = CLOCKS_PER_SEC / 1000; // clock per milliseconds
     int nums1[NUMBERS_SIZE];
     int nums2[NUMBERS_SIZE];
     int nums3[NUMBERS_SIZE];
     fillArrays(nums1, nums2, nums3);
     cout << "testing arrays of size " << NUMBERS_SIZE << '\n';
 
-    const int CLOCKS_PER_MS = CLOCKS_PER_SEC / 1000; // clock per milliseconds
-    //testing insertion sort time
+    // testing insertion sort time
     clock_t Start = clock();
     InsertionSort(nums3, NUMBERS_SIZE);
     clock_t End = clock();
     int elapsedTime = (End - Start) / CLOCKS_PER_MS; // converts elapsed time from microseconds to milliseconds.
     cout << "insertion sort time: " << elapsedTime << "ms\n";
 
-    //testing Quicksort (midpoint) time
+    // testing Quicksort (midpoint) time
     Start = clock();
     Quicksort_midpoint(nums1, 0, NUMBERS_SIZE);
     End = clock();
     elapsedTime = (End - Start) / CLOCKS_PER_MS;
     cout << "Quicksort (midpoint) time: " << elapsedTime << "ms\n";
 
-    //testing Quicksort (median of three) time
+    // testing Quicksort (median of three) time
     Start = clock();
     Quicksort_medianOfThree(nums1, 0, NUMBERS_SIZE);
     End = clock();
     elapsedTime = (End - Start) / CLOCKS_PER_MS;
     cout << "Quicksort (median of three) time: " << elapsedTime << "ms\n";
 
+    cout << "\ncalling sort on the sorted arrays:\n";
 
+    // testing insertion sort time
+    Start = clock();
+    InsertionSort(nums3, NUMBERS_SIZE);
+    End = clock();
+    elapsedTime = (End - Start) / CLOCKS_PER_MS;
+    cout << "insertion sort time: " << elapsedTime << "ms\n";
+
+    // testing Quicksort (midpoint) time
+    Start = clock();
+    Quicksort_midpoint(nums1, 0, NUMBERS_SIZE);
+    End = clock();
+    elapsedTime = (End - Start) / CLOCKS_PER_MS;
+    cout << "Quicksort (midpoint) time: " << elapsedTime << "ms\n";
+
+    // testing Quicksort (median of three) time
+    Start = clock();
+    Quicksort_medianOfThree(nums1, 0, NUMBERS_SIZE);
+    End = clock();
+    elapsedTime = (End - Start) / CLOCKS_PER_MS;
+    cout << "Quicksort (median of three) time: " << elapsedTime << "ms\n";
 }
 
 // returns a random inclusive int
@@ -62,32 +84,91 @@ void fillArrays(int arr1[], int arr2[], int arr3[])
     }
 }
 
-/*sorts the array using insertion sort
- algorithm: 
-    outer loop: point to start of unsorted portion of arraay
-    inner loop: shift elements to the left into its proper place in the sorted portion of the array
+/*
+    sorts the array using insertion sort
+    algorithm:
+        outer loop: point to start of unsorted portion of arraay
+        inner loop: shift elements to the left into its proper place in the sorted portion of the array
 */
 void InsertionSort(int numbers[], int numbersSize)
 {
     for (int i = 0; i < numbersSize - 1; i++)
     {
         int j = i;
-        //swap elements until key is put in right place 
+        // swap elements until key is put in right place
         while (numbers[j] > numbers[j + 1] && j >= 0)
         {
-            auto swapHolder = numbers[j];
+            int swapHolder = numbers[j];
             numbers[j] = numbers[j + 1];
-            numbers[j + 1] = swapHolder; 
+            numbers[j + 1] = swapHolder;
             j--;
         }
     }
 }
 
-void Quicksort_midpoint(int numbers[], int i, int k)
+/*
+    Sorts the given array in the range from i to k using quicksort method.
+    In this function, pivot is the midpoint element (numbers[(i+k)/2]).
+*/
+void Quicksort_midpoint(int numbers[], int low, int high)
 {
-    cout << "do Quicksort_midpoint\n";
+    if (low >= high) return;
+
+    int lowPartitionHigh = Partition(numbers, low, high, false);
+    Quicksort_midpoint(numbers, low, lowPartitionHigh);
+    Quicksort_midpoint(numbers, lowPartitionHigh + 1, high);
 }
-void Quicksort_medianOfThree(int numbers[], int i, int k)
+
+/*
+    This quicksort is the same as the other one but uses a different pivot
+    The pivot is the median of the following three values:
+        leftmost (numbers[i]), midpoint (numbers[(i+k)/2]) and rightmost (numbers[k]).
+*/
+void Quicksort_medianOfThree(int numbers[], int low, int high)
 {
-    cout << "do Quicksort_medianOfThree\n";
+    if (low >= high) return;
+
+    int lowPartitionHigh = Partition(numbers, low, high, true);
+    Quicksort_midpoint(numbers, low, lowPartitionHigh);
+    Quicksort_midpoint(numbers, lowPartitionHigh + 1, high);
+}
+
+/*
+    Partitions array and is used in quicksort.
+    Returns the highest index of the low partition.
+*/
+int Partition(int numbers[], int low, int high, bool usesMedianOfThree)
+{
+    int pivotIndex = low + (high - low) / 2;
+    int pivot = numbers[pivotIndex];
+    if (usesMedianOfThree)
+    {
+        // gets the median of the first, middle, and last elements
+        // uses actual black magic
+        pivot = max(min(numbers[low], numbers[pivotIndex]), min(max(numbers[low], numbers[pivotIndex]), numbers[high]));
+    }
+    while (true)
+    {
+        while (numbers[low] < pivot)
+        {
+            low++;
+        }
+        while (pivot < numbers[high])
+        {
+            high--;
+        }
+        // this is true when all elements are partitioned (0 or 1 elements remain)
+        if (low >= high)
+        {
+            break;
+        }
+        //swaps numbers at low and high index
+        int swapHolder = numbers[low];
+        numbers[low] = numbers[high];
+        numbers[high] = swapHolder;
+        //move low and high so we don't look at the same elements again
+        low++;
+        high--;
+    }
+    return high;
 }
